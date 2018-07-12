5406eb1b11ed9fee01f92e57d45256eaff2aae32
Nathan Lin <nlin@theticker.org>
2018-07-12 19:32:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,8 +25,7 @@ function accessElementInArray(array, index) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-  array.slice(1)
-  return array
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromBeginningOfArray (array) {
